d0838d767ee04e1a114747d6e3e0e3318e22f632
Vicolas11 <incrediblechamp1@gmail.com>
2024-04-29 11:44:53 +0100

    refactor: add competition and quiz routers

--- src/routers/competition.router.ts
+++ src/routers/competition.router.ts
@@ -1,10 +1,88 @@
+import CreateCompetitionController from "../controllers/competition/createCompetitionController";
+import DeleteCompetitionController from "../controllers/competition/deleteCompetitionController";
+import GetAllCompetitionController from "../controllers/competition/getAllCompetitionController";
+import JoinedCompetitionController from "../controllers/competition/joinedCompetitionController";
+import SubmitCompetitionController from "../controllers/competition/submitCompetitionController";
+import UpdateCompetitionController from "../controllers/competition/updateCompetitionController";
+import GetCompetitionController from "../controllers/competition/getCompetitionController";
+import GetLeaderboardController from "../controllers/competition/getLeaderboardController";
 import { authenticateToken } from "../middlewares/authenticateToken";
 import { validateRequest } from "../middlewares/validateRequest";
 import { Router } from "express";
+import {
+  CompetitionBoardSchema,
+  CompetitionInputSchema,
+  CompetitionParamsSchema,
+  CompetitionQuerySchema,
+  CompetitionSubInpSchema,
+  CompetitionUpdInputSchema,
+} from "../joi/competition.joi";
+import FeedbackController from "../controllers/competition/FeedbackController";
+import { valid } from "joi";
+import GetJoinedCompetitionController from "../controllers/competition/getJoinedCompetitionController";
 
 const competitionRouters = Router();
 
 competitionRouters.use(authenticateToken);
 
+competitionRouters.post(
+  "/competitions",
+  validateRequest(CompetitionInputSchema),
+  CreateCompetitionController
+);
+
+competitionRouters.get(
+  "/competitions",
+  validateRequest(CompetitionQuerySchema, "query"),
+  GetAllCompetitionController
+);
+
+competitionRouters.get(
+  "/competitions/joined",
+  validateRequest(CompetitionQuerySchema, "query"),
+  GetJoinedCompetitionController
+);
+
+competitionRouters.get(
+  "/competitions/:id",
+  validateRequest(CompetitionParamsSchema, "params"),
+  GetCompetitionController
+);
+
+competitionRouters.patch(
+  "/competitions",
+  validateRequest(CompetitionUpdInputSchema),
+  UpdateCompetitionController
+);
+
+competitionRouters.delete(
+  "/competitions/:id",
+  validateRequest(CompetitionParamsSchema, "params"),
+  DeleteCompetitionController
+);
+
+competitionRouters.post(
+  "/competitions/:id/join",
+  validateRequest(CompetitionParamsSchema, "params"),
+  JoinedCompetitionController
+);
+
+competitionRouters.patch(
+  "/competitions/submit",
+  validateRequest(CompetitionSubInpSchema),
+  SubmitCompetitionController
+);
+
+competitionRouters.get(
+  "/feedback/:id",
+  validateRequest(CompetitionParamsSchema, "params"),
+  FeedbackController
+);
+
+competitionRouters.get(
+  "/competitions/:id/leaderboard",
+  validateRequest(CompetitionBoardSchema, "params"),
+  GetLeaderboardController
+);
 
 export default competitionRouters;
--- src/routers/quiz.router.ts
+++ src/routers/quiz.router.ts
@@ -1,8 +1,14 @@
 import CreateQuizController from "../controllers/quiz/createQuizController";
-import { Router } from "express";
-import { validateRequest } from "../middlewares/validateRequest";
-import { QuizInputSchema } from "../joi/quiz.joi";
+import SubmitQuizController from "../controllers/quiz/submitQuizController";
+import GetQuizController from "../controllers/quiz/getQuizController";
 import { authenticateToken } from "../middlewares/authenticateToken";
+import { validateRequest } from "../middlewares/validateRequest";
+import { Router } from "express";
+import {
+  QuizIdParamSchema,
+  QuizInputSchema,
+  QuizSubmitInputSchema,
+} from "../joi/quiz.joi";
 
 const quizRouters = Router();
 
@@ -12,6 +18,18 @@ quizRouters.post(
   "/quiz",
   validateRequest(QuizInputSchema),
   CreateQuizController
+);
+
+quizRouters.get(
+  "/quiz/:id",
+  validateRequest(QuizIdParamSchema, "params"),
+  GetQuizController
+);
+
+quizRouters.post(
+  "/quiz/submit",
+  validateRequest(QuizSubmitInputSchema),
+  SubmitQuizController
 );
 
 export default quizRouters;
