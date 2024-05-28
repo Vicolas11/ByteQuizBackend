0a3905e3fc561b7f0aecb1bbf8561261e78c6f76
Vicolas11 <incrediblechamp1@gmail.com>
2024-05-28 09:36:34 +0100

    refactor: remove unused constant variable

--- src/configs/constant.config.ts
+++ src/configs/constant.config.ts
@@ -3,13 +3,11 @@ import { config } from "dotenv";
 config();
 
 export const constant = {
-  expiresIn: "30m",
-  refreshIn: "1h",
+  expiresIn: "1h",
+  refreshIn: "2h",
   secretKey: process.env.SECRET_KEY as string,
   maleUrl: process.env.AVA_MALE_URL,
   femaleUrl: process.env.AVA_FEMALE_URL,
   coverImgUrl: process.env.COVER_IMG_URL,
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
   medalImgUrl: process.env.MEDAL_URL
 };
