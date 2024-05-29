c9bf3e811ce4799faa957fd03e69963ddc8101f8
Vicolas11 <incrediblechamp1@gmail.com>
2024-05-29 09:17:04 +0100

    refactor: validate uuid request

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { removeSlash } from "../utils/removeslash.utils";
 import { errorResponse } from "../utils/errorResponse";
+import { validate as isUUID } from "uuid";
 import Joi from "joi";
 
 export const validateRequest = (
@@ -9,6 +10,7 @@ export const validateRequest = (
 ) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req[reqKey || "body"]);
+    const { id: idParams } = req.params;
 
     if (error) {
       return errorResponse({
@@ -16,6 +18,15 @@ export const validateRequest = (
           removeSlash(error.details[0].message) ||
           `${error.details.map((err) => removeSlash(err.message))}` ||
           "Invalid input",
+        status: 400,
+        res,
+      });
+    }
+
+    // Validate the UUID format
+    if (idParams && !isUUID(idParams)) {
+      return errorResponse({
+        message: "Invalid ID format!",
         status: 400,
         res,
       });
